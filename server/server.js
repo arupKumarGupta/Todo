7497cc39b35de29993238a91710e3557496cc58e
Arup Kumar Gupta <arup.professional@gmail.com>
2018-05-12 11:14:47 +0530

    added get route to fetch todos using id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
-const {mongoose} = require('./db/mongo');
-const {Todo} = require('./models/Todo');
-const {User} = require('./models/User');
-
+const {
+    mongoose,
+    ObjectID
+} = require('./db/mongo');
+const {
+    Todo
+} = require('./models/Todo');
+const {
+    User
+} = require('./models/User');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,25 +15,47 @@ var app = express();
 app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 
-app.post('/todos',(req,res)=>{
-    var todo = new Todo({text:req.body.text});
-    todo.save().then((doc)=>{
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e);
     });
 });
 
-app.get('/todos',(req,res)=>{
-    var todos = Todo.find().then((todos)=>{
-        res.send({todos});
-    },(error)=>{
+app.get('/todos', (req, res) => {
+    var todos = Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (error) => {
         res.status(400).send(error);
     });
 });
 
-app.listen(port,()=>{
-    console.log(`Started server on port ${port}`);
-    
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+    Todo.findById(id).then((todo) => {
+        if (!todo)
+            return res.status(404).send();
+        res.status(200).send({
+            todo
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
-module.exports={app};
+
+app.listen(port, () => {
+    console.log(`Started server on port ${port}`);
+
+});
+module.exports = {
+    app
+};
