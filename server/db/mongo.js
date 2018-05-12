0b9200e35b388d65bdd3b2279c3b2a00741ffbc2
Arup Kumar Gupta <arup.professional@gmail.com>
2018-05-12 11:13:28 +0530

    added ObjectID to mongo

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const {
+    ObjectID
+} = require('mongodb');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
-module.exports = {mongoose};
+module.exports = {
+    mongoose,
+    ObjectID
+};
