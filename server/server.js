dc068fde18a6bf161e780167a0a118765810fd24
Arup Kumar Gupta <arup.professional@gmail.com>
2018-05-13 21:45:31 +0530

    addded update todo route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {
     mongoose,
     ObjectID
@@ -63,8 +64,37 @@ app.delete('/todos/:id', (req, res) => {
             todo
         });
     }).catch((e) => {
-        res.status(404).send()
+        res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {
+        $set: body,
+
+    }, {
+        new: true,
+        runValidators: true
+    }).then((todo) => {
+        if (!todo)
+            return res.status(404).send();
+        res.status(200).send({
+            todo
+        });
+    }).catch((e) => {
+        return res.status(400).send();
+    });
+
 });
 app.listen(port, () => {
     console.log(`Started server on port ${port}`);
