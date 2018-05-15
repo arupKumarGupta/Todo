447f35db13eaadfd35eb4a2351ddeb876dd8665b
Arup Kumar Gupta <arup.professional@gmail.com>
2018-05-15 09:14:30 +0530

    created post /users route

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,40 @@
 const mongoose = require('mongoose');
-var User = mongoose.model('Users',{
-    email:{
+const validator = require('validator');
+
+/* u = {
+    email: 'x@g.com',
+    password: "hashed",
+    tokens: [{
+        access:'auth',
+        token: 'hashed',
+    }]
+} */
+var User = mongoose.model('Users', {
+    email: {
         type: String,
-        required:true,
-        minlength:1,
-        trim: true
-    }
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        required: true
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
-module.exports = {User};
+module.exports = {
+    User
+};
